9a05becec62da517c996b742658c6575ce3d6bf1
Baaba Onomaa Amosah <146720493+baaba-midnight@users.noreply.github.com>
2024-12-18 12:20:42 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 $servername = '169.239.251.102';
 $username = 'baaba.amosah';
 $dbname = 'webtech_fall2024_baaba_amosah';
-$password = 'Andriod17@CS';
+$password = '';
 
 $conn = new mysqli(
     $servername,
